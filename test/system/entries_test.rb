b2f49253f47c04c01c31d68a288bc9e6cd6b2350
ozzo <ozzo@nuc.local>
2026-07-21 10:22:24 +0200

    Anchor link validation regex, add system tests
    Brakeman flagged the link format regex for missing an end anchor
    (\A...\z). Also add the missing test/system suite CI already runs:
    public feed loads unauthenticated, new-entry redirects to sign-in.

---
+++ test/system/entries_test.rb
@@ -0,0 +1,16 @@
+require "application_system_test_case"
+
+class EntriesTest < ApplicationSystemTestCase
+  test "visiting the public feed without signing in" do
+    visit entries_url
+
+    assert_selector "h1", text: "buildlog"
+    assert_text entries(:one).body
+  end
+
+  test "new entry requires sign in" do
+    visit new_entry_url
+
+    assert_selector "h1", text: "Sign in"
+  end
+end
